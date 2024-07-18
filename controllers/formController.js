d817a0014fdc51414ca9c931030854229560d89b
= <=>
2024-07-18 18:44:55 +0530

    Integrated mysql with node

--- controllers/formController.js
+++ controllers/formController.js
@@ -1,7 +1,19 @@
 //controllers/formController.js
 exports.saveFormData = (req, res) => {
-    res.json({
-        status: 'success',
-        data: {...req.body}
+    const {name, email, message} = req.body;
+    const pool = req.pool;
+    console.log("Pool -", pool);
+    const sql = `INSERT INTO form_data (name, email, message) VALUES (?, ?, ?)`;
+    req.pool.query(sql, [name, email, message], (err, results) => {
+        console.log("Result: " + JSON.stringify(results));
+        if(err) {
+            console.error(err);
+            res.status(500).json({ error: 'Error saving form data' });
+            return;
+        }
+        res.json({
+            status: 'success',
+            data: {...req.body}
+        })  
     })
 }
--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('node:path');
+const mysql = require('mysql2');
 const weatherRoutes = require('./routes/weatherRoutes');
 const userRoutes = require('./routes/userRoutes');
 const dynamicWebpageRoutes = require('./routes/dynamicWebpageRoutes');
@@ -11,13 +12,25 @@ const port = 3000;
 
 app.use(bodyParser.urlencoded({extended: false})); // to parse the form data
 app.use(bodyParser.json()); // Always Always keep it on top of middleware
+const pool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: 'Password@123',
+    database: 'myapp_db'
+})
+
+app.use((req, res, next) => {
+    req.pool = pool;
+    next();
+})
+
+
+
 app.use('/weather', weatherRoutes);
 app.use('/user', userRoutes);
 // app.use('/', dynamicWebpageRoutes);
 app.use('/submit-form', formRoutes)
 app.use('/', express.static(path.join(__dirname, 'public')));
-
-
 
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
